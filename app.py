a19d1681d302fd87f9ebe5d1d45fcdc4d1a97778
JWTownhill <johntownhill@gmail.com>
2021-03-28 16:05:31 +0100

    html

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from fastai.vision.all import *
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config.from_object(os.environ["APP_SETTINGS"])
@@ -9,13 +9,14 @@ path = Path()
 learn = load_learner(path / "model/export.pkl")
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return "Hello world"
+    return render_template("index.html")
 
-#img = PILImage.create(btn_upload.data[-1])
-#pred, pred_idx, probs = learn.predict(img)
-#lbl_pred.value = f'Prediction: {pred} egg; Probability: {probs[pred_idx]:.02f}'
+
+# img = PILImage.create(btn_upload.data[-1])
+# pred, pred_idx, probs = learn.predict(img)
+# lbl_pred.value = f'Prediction: {pred} egg; Probability: {probs[pred_idx]:.02f}'
 
 if __name__ == "__main__":
     app.run()
